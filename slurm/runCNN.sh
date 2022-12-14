5d325c42f578a20e9f5815165ecd210107601e07
Albert-Saporta <albert.saporta@outlook.com>
2022-12-14 11:42:31 +0100

    slurm

--- slurm/runCNN.sh
+++ slurm/runCNN.sh
@@ -2,15 +2,15 @@
 
 #SBATCH --job-name=Radiomics   # name of the job
 #SBATCH --partition=casus    # partition to be used (defq, gpu or intel)
-#SBATCH --time=19:00:00       # walltime (up to 96 hours)
-#SBATCH --mem 70000
+#SBATCH --time=47:00:00       # walltime (up to 96 hours)
+#SBATCH --mem 100000
 #SBATCH -p gpu
-##SBATCH -C v100-32g
-#SBATCH --gres=gpu:1
 #SBATCH --nodes=1          # number of nodes
+##SBATCH -C v100-32g
+#SBATCH --gres=gpu:32
 #SBATCH --ntasks=1     # number of tasks (i.e. parallel processes) to be started
-#SBATCH --output=Radiomics%j.out      # nom du fichier de sortie
-#SBATCH --error=Radiomics%j.out       # nom du fichier d'erreur (ici commun avec la sortie)
+#SBATCH --output=radiomics%j.out      # nom du fichier de sortie
+#SBATCH --error=radiomics%j.out       # nom du fichier d'erreur (ici commun avec la sortie)
 
 module purge
 
